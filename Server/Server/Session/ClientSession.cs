9925faf39b0cd370c8fd856140884e600c31e307
DESKTOP-4D15S2R\ekswl <ekswlsgud@gmail.com>
2022-11-05 17:29:17 +0900

    20221105_1729 - 패킷모아보내기 50으로 줄임

--- Server/Server/Session/ClientSession.cs
+++ Server/Server/Session/ClientSession.cs
@@ -146,7 +146,7 @@ namespace Server
 				// 한텀 기다리고 모아보내기 20221031 추가
 				// 0.1초가 지났거나, 너무 패킷이 많이 모일 때 ( 1만 바이트)
 				long delta = (System.Environment.TickCount64 - _lastSendTick);
-                if (delta < 100 && _reservedSendBytes < 10000)
+                if (delta < 50 && _reservedSendBytes < 10000)
                     return;
 
                 // 패킷 모아 보내기

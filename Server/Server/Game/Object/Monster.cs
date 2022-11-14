b46611a97abcf617bd29295ceb6194377fa4aac1
DESKTOP-4D15S2R\ekswl <ekswlsgud@gmail.com>
2022-11-14 20:04:13 +0900

    20221114_2003 - 몬스터 바로 때리게 수정 (느리게 걸으나 바로 때리게 )

--- Server/Server/Game/Object/Monster.cs
+++ Server/Server/Game/Object/Monster.cs
@@ -53,7 +53,7 @@ namespace Server.Game
 
             State = CreatureState.Idle;
 
-            updateTime = 1000;
+            updateTime = 200;
         }
 
 
@@ -62,7 +62,7 @@ namespace Server.Game
 
         IJob _job;
 
-        int updateTime = 1000;
+        int updateTime = 200;
 
         public override void Update()
         {
@@ -114,7 +114,7 @@ namespace Server.Game
 
             if (_nextSearchTick > Environment.TickCount64)
                 return;
-            _nextSearchTick = Environment.TickCount64 + 200; // 0.2초가 지났으면 0.2초더 증가시켜준다.
+            _nextSearchTick = Environment.TickCount64 + 3000; // 0.2초가 지났으면 0.2초더 증가시켜준다.
 
             // 이미 타겟이 있는 경우에는 거리를 보지 않는다.
 

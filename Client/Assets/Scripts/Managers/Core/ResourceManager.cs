29e9482366169b77be8309b714ceb589e3023ec0
DESKTOP-4D15S2R\ekswl <ekswlsgud@gmail.com>
2022-11-19 20:39:02 +0900

    20221119_2038 - Destroy 시간 추가 (poolable 대비 )

--- Client/Assets/Scripts/Managers/Core/ResourceManager.cs
+++ Client/Assets/Scripts/Managers/Core/ResourceManager.cs
@@ -59,14 +59,26 @@ public class ResourceManager
         if (go == null)
             return;
 
+        Managers.Instance.StartCoroutine(coDestory(go, time));
+    }
+
+    // 시간지나면 지워지게 (풀링때문에 넣음)
+    IEnumerator coDestory(GameObject go, float time)
+    {
+        yield return new WaitForSeconds(time);
+
+
         Poolable poolable = go.GetComponent<Poolable>();
         if (poolable != null)
         {
             Managers.Pool.Push(poolable);
-            return;
+     
+        }
+        else
+        {
+            Object.Destroy(go, time);
         }
 
-        Object.Destroy(go,time);
     }
 
 
